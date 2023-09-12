75a6a68232af8439d426dab54b78f6bbc8530a73
yamini1526 <144451163+yamini1526@users.noreply.github.com>
2023-09-12 18:33:12 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
-# Python program to find the factorial of a number provided by the user.
-
-# change the value for a different result
-num = 10
-
-# To take input from the user
-#num = int(input("Enter a number: "))
-
-factorial = 1
-
-# check if the number is negative, positive or zero
-if num < 0:
-   print("Sorry, factorial does not exist for negative numbers")
-elif num == 0:
-   print("The factorial of 0 is 1")
+# Python program to check if year is a leap year or not
+year = 2000
+if (year % 400 == 0) and (year % 100 == 0):
+    print("{0} is a leap year".format(year))
+elif (year % 4 ==0) and (year % 100 != 0):
+    print("{0} is a leap year".format(year))
 else:
-   for i in range(1,num + 1):
-       factorial = factorial*i
-   print("The factorial of",num,"is",factorial)
+    print("{0} is not a leap year".format(year))
